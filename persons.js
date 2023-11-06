4d4bc1ace0f89c0fea7dc27f22249be5c058e6bf
Nosipho19 <hanyanenosipho@gmail.com>
2023-11-06 15:16:55 +0200

    updated

---
+++ persons.js
@@ -0,0 +1,27 @@
+function persons (name,age){
+this.name = name;
+this.age = age; 
+}
+//}} then we we create two persons object
+
+let mary = new persons ("mary", 32);
+let Joseph = new persons ("Joseph", 20);
+
+
+
+console.log(mary);
+console.log(Joseph);
+
+let marlo ={
+    'name': 'marlo',
+    'age': '20'
+}
+ function students(name,age,course){
+ this.name = name
+ this.name = age
+ this.name = course
+ }
+  
+ let lydia = new students ( marlo, 24, 'business')
+ console.log(lydia)
+
