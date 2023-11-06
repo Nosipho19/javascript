78847a8911f9b5e0b7ee6172e99c0b73422cc5ef
Nosipho19 <hanyanenosipho@gmail.com>
2023-11-06 11:54:20 +0200

    modified

---
+++ arraymod.js
@@ -0,0 +1,8 @@
+let courses = ["java", "ruby", "rue"]
+
+function func(elements){
+console.log(elements);
+
+}
+
+courses ForEach(func)
--- arrays.js
+++ arrays.js
@@ -1,15 +1,33 @@
 let courses = ["Angular", "javascript", "html", "React" ]; // pre defined array
 
 
-
-
-courses.length = 8;
+courses.length = 5;
 courses [3]= "Typescript";
-courses [5] = "flutter"
-console.log(courses);
-console.log(courses.toString())
 
+courses [5] = "flutter";
+
+console.log("courses available" + courses );
+console.log("typeof coureses"); //if you want the array to return object
+courses.push("C#", "python", "C/C++", "Svelte");
+console.log("new seamaster courses" + courses);
+
+//when you want an array to return strictly as an array
+console.log(courses instanceof Array);
+
+
+console.log(courses.toString());
+console.log(courses);
+
+
+   
 let learners = new Array (3);
+
+learners [0] = 1;
+learners [1] = 2;
+learners [2] = 3;
+learners [3] = 4;
+// learners[3 = 4]
+
 let i;
 let len = courses.len
 let space = " "
@@ -19,20 +37,10 @@ for (; i< len;){
      // the += allows the elements to be printed
     i++; 
     }
-   
-
-
-learners [0] = 1;
-learners [1] = 2;
-learners [2] = 3;
-learners [3] = 4;
-// learners[3 = 4]
 
 
 console.log(space);
-console.log("courses available" + courses )
-console.log("new seamaster courses" + courses)
-courses.push("C#", "python", "C/C++", "Svelte")
-console.log("number of learners:" + learners )
+
+console.log("number of learners:" + learners );
 
 
