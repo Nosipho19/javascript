2707bdde884554323315abd53c7826bf36dfa0f9
Nosipho19 <hanyanenosipho@gmail.com>
2023-11-06 14:43:27 +0200

    functions

---
+++ constructions.js
@@ -0,0 +1,18 @@
+const student = {
+    // data property
+    firstName: 'Thendo'
+    // surname: 'white'
+    //accessor property(getter)
+    ,
+
+
+
+    //accessor property(getter)
+    get getName() {
+        return this.firstName;
+
+        set; changeName('NewName'); {
+            this.firstName = NewName;
+        }
+    }
+};
